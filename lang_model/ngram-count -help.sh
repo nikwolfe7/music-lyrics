65d866f49927707a547741ae15d7b39218bcb0b5
nikwolfe7 <nikwolfe7@gmail.com>
2015-03-25 01:34:00 -0400

    asdfasdf

---
+++ lang_model/ngram-count -help.sh
@@ -0,0 +1,11 @@
+VOCAB=".nostop.vocab"
+TRAIN=".train.nostop.txt"
+TEST=".test.nostop.txt"
+BROWN="brown"
+HHR="hip_hop_rap"
+RMC="rock_metal_country"
+SONNETS="shakespeare_sonnets"
+SHAKESPEARE="shakespeare"
+LE_FABLIAUX="le_fabliaux"
+
+ngram-count -help
